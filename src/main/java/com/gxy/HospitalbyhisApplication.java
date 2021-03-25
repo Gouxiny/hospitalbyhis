245e0835bdb5b8135cdbcece0f7c51112b9eb2ea
gxy <364911945@qq.com>
2021-03-25 17:47:59 +0800

    提交

---
+++ src/main/java/com/gxy/HospitalbyhisApplication.java
@@ -0,0 +1,13 @@
+package com.gxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalbyhisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalbyhisApplication.class, args);
+	}
+
+}
